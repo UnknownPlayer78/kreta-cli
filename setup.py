960a97de42d55d7b39d827ec9b32e182d033aefb
UnknownPlayer78 <nyitraiszabolcs4@gmail.com>
2020-04-10 23:49:13 +0200

    setup.py additions

--- setup.py
+++ setup.py
@@ -12,10 +12,15 @@ setup(
     long_description="",
     author='UnknownPlayer78',
     author_email='',
+    url='https://github.com/UnknownPlayer78/kreta-cli',
     license="MIT",
     keywords="e-kreta kreta cli kreta-cli",
     packages=['kreta'],
     scripts=[
         'bin/kreta',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
     ]
 )
